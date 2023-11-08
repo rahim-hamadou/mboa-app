6dfaaa868f5e967dea33d054f4ff29fcc008c15b
rumgal <rumgal@gmail.com>
2023-11-08 22:55:25 +0100

    add routes

--- scamboa/frontend/index.js
+++ scamboa/frontend/index.js
@@ -34,3 +34,69 @@ fetch("http://localhost:3000/users")
 			ulBack.appendChild(li);
 		});
 	});
+
+// post to backend
+const dataUser = {
+	nom: "paul",
+};
+
+// recup name
+const inputName = document.querySelector(".inputName");
+
+// const check = function () {
+// 	const userName = {
+// 		nom: inputName.value,
+// 	};
+// 	console.log(userName);
+// };
+const addUser = (user) => {
+	const userName = {
+		nom: inputName.value,
+	};
+	// const userName = user.value;
+	fetch("http://localhost:3000/users/adduser", {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+
+		body: JSON.stringify(userName),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("userName:", userName);
+			console.log("data:", data);
+			window.location.reload();
+		});
+};
+
+// update last
+const updateUser = (user) => {
+	const userName = {
+		nom: inputName.value,
+	};
+	// const userName = user.value;
+	fetch("http://localhost:3000/users/update/:position", {
+		method: "UPDATE",
+		headers: { "Content-Type": "application/json" },
+
+		body: JSON.stringify(userName),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("userName:", userName);
+			console.log("data:", data);
+			window.location.reload();
+		});
+};
+
+// delete last one
+const removeUser = () => {
+	fetch("http://localhost:3000/users/deletelast", {
+		method: "DELETE",
+		headers: { "Content-Type": "application/json" },
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("dataToErase:", data);
+			window.location.reload();
+		});
+};
